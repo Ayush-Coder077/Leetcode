bd08f3af56c3ff6469881a92b86ef7105fefc95f
Ayush Datkhile <datkhileayush@gmail.com>
2026-09-05 20:25:51 +0530

    Time: 0 ms (100%), Space: 42 MB (57.92%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles ;
+
+        while(numBottles>=numExchange){
+            int newBottles = numBottles / numExchange;
+            int rb = numBottles % numExchange;
+            ans = ans + newBottles;
+            numBottles = newBottles + rb;
+        }
+        return ans;
+    }
+}
